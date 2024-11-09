31a08102fca75eca90bc199768267627f7bd3d42
Brian LeMaster <brian.lemaster@outlook.com>
2024-11-09 14:24:14 -0500

    Add project files.

---
+++ ConsoleMenus.UI/Menu.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ConsoleMenus.UI;
+
+internal record Menu(string Title, IEnumerable<string> MenuOptions, IEnumerable<ConsoleKey> ValidKeys, string ValidationError, ConsoleKey SelectedKey)
+{
+    internal IEnumerable<Menu> SubMenus { get; init; } = [];
+    internal ConsoleKey SelectedKey { get; private set; } = SelectedKey;
+
+    internal ConsoleKey GetKey() => SelectedKey = Console.ReadKey().Key;
+    internal bool SelectedKeyIsValid() => ValidKeys.Contains(SelectedKey);
+}
